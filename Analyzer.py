a7c2b189f07bbe6be3027651d1af5926eb1d89d7
Jorge Alonso Hernández <alu0100767803@ull.edu.es>
2018-03-20 19:19:28 +0000

    Realizando la extracción de datos

--- Analyzer.py
+++ Analyzer.py
@@ -22,6 +22,22 @@ params = {'access_token' : 'EAACQFfa9JeIBAKK0ZCX9x7vGxtDjfDjLLo5W8qVz5REnvqr30DS
 posts = requests.get(page_url, params = params)
 posts = posts.json()
 
-("""print(data)
-for element in data['data']:
-   print(element['message'])""")
+while True:
+    try:
+    ### Recupera un post
+        for element in posts['data']:
+            collection_posts.insert(element)
+            #### Recupera todos los comentarios de ese post
+            this_comment_url = comments_url.replace("{post_id}", element['id'])
+            comments = requests.get(this_comment_url, params = params).json()
+            # Recorre todos los comentarios hasta que la respuesta esté vacía, que no haya más comentarios
+            while ('paging' in comments and 'cursors' in comments['paging'] and 'after' in comments['paging']['cursors']):
+                ### Itera a través de todos los comentarios
+                for comment in comments['data']:
+                    comment['post_id'] = element['id']
+                    collection_comments.insert(comment)
+                comments = requests.get(this_comment_url + '&after' + comments['paging']['cursors']['after'], params = params).json()
+        ####Vamos a la siguiente página en feed
+        posts = requests.get(posts['paging']['next']).json()
+    except KeyError:
+        break
